f0f36cbedb7394f6140caf9ac3e4eb79392e58df
ChiragMangukia <chirag.mangukia@gmail.com>
2018-05-29 19:59:37 +0530

    Logging added.

--- GmailLogin.py
+++ GmailLogin.py
@@ -1,10 +1,21 @@
 import time
+import logging
 import MyMethods
 
 
-driver = MyMethods.invokeChrome()
-driver.get("https://www.gmail.com")
-driver.find_element_by_id("identifierId").send_keys("adcdefg777")
+log_filename = 'D:/logger.log'
+logging.basicConfig(filename=log_filename, level=logging.DEBUG)
 
+
+driver = MyMethods.invokeFirefox()
+logging.debug('Chrome browser invoked')
+logging.debug('Broswer Version' + driver.capabilities['version'])
+driver.get("https://www.gmail.com")
+logging.debug('Gmail opened')
+driver.find_element_by_id("identifierId").send_keys("adcdefg777")
+logging.debug('Entered Email Id')
+time.sleep(3)
+logging.debug('Slept for 3 seconds')
+driver.quit()
 
 MyMethods.killProcess()
