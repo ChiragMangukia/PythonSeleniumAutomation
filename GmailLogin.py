68c872e144fe56926e85d56dce9fb3589322ca37
ChiragMangukia <chirag.mangukia@gmail.com>
2018-05-31 20:56:00 +0530

    Commit from Bash

--- GmailLogin.py
+++ GmailLogin.py
@@ -2,20 +2,19 @@ import time
 import logging
 import MyMethods
 
+FIREFOX = MyMethods.invokeFirefox()
 
 log_filename = 'D:/logger.log'
 logging.basicConfig(filename=log_filename, level=logging.DEBUG)
 
-
 driver = MyMethods.invokeFirefox()
-logging.debug('Chrome browser invoked')
-logging.debug('Broswer Version' + driver.capabilities['version'])
+logging.debug('Chrome browser invoked Broswer Version' + driver.capabilities['version'])
 driver.get("https://www.gmail.com")
 logging.debug('Gmail opened')
 driver.find_element_by_id("identifierId").send_keys("adcdefg777")
 logging.debug('Entered Email Id')
-time.sleep(3)
-logging.debug('Slept for 3 seconds')
+time.sleep(4)
+logging.debug('Slept for 4 seconds')
 driver.quit()
 
 MyMethods.killProcess()
