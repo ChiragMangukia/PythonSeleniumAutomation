1147473c31401ebcb8ad882c79a6f6f1900583aa
ChiragMangukia <chirag.mangukia@gmail.com>
2018-05-31 21:04:42 +0530

    From Web

--- FacebookLogin.py
+++ FacebookLogin.py
@@ -5,7 +5,7 @@ import MyMethods
 driver = MyMethods.invokeChrome()
 driver.get("https://www.facebook.com")
 driver.find_element_by_id("email").send_keys("adcdefg777@gmail.com")
-driver.find_element_by_id("pass").send_keys("lovely@06")
+driver.find_element_by_id("pass").send_keys("lovely@00")
 driver.find_element_by_xpath("//input[@value='Log In']").click()
 driver.quit()
 MyMethods.killProcess()
