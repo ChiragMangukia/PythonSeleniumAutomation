aa3b6a1b03f2a7ad99a202baeed3f16ff8febb33
ChiragMangukia <chirag.mangukia@gmail.com>
2018-06-13 20:58:50 +0530

    Removed unused variable and changed URL

--- GmailLogin.py
+++ GmailLogin.py
@@ -2,19 +2,16 @@ import time
 import logging
 import MyMethods
 
-FIREFOX = MyMethods.invokeFirefox()
 
 log_filename = 'D:/logger.log'
 logging.basicConfig(filename=log_filename, level=logging.DEBUG)
 
-<<<<<<< HEAD
-
 driver = MyMethods.invokeChrome()
 logging.debug('Chrome browser invoked')
-logging.debug('Broswer Version ' + driver.capabilities['version'])
+logging.debug('Browser Version ' + driver.capabilities['version'])
 
 try:
-    driver.get("https://www.gmail.com")
+    driver.get("https://accounts.google.com")
     logging.debug('Gmail opened')
     driver.find_element_by_id("identifierId").send_keys("adcdefg777")
     logging.debug('Entered Email Id')
@@ -27,17 +24,3 @@ try:
 except  Exception as e:
     logging.debug(e)
     MyMethods.killProcess()
-
-=======
-driver = MyMethods.invokeFirefox()
-logging.debug('Chrome browser invoked Broswer Version' + driver.capabilities['version'])
-driver.get("https://www.gmail.com")
-logging.debug('Gmail opened')
-driver.find_element_by_id("identifierId").send_keys("adcdefg777")
-logging.debug('Entered Email Id')
-time.sleep(4)
-logging.debug('Slept for 4 seconds')
-driver.quit()
-
-MyMethods.killProcess()
->>>>>>> 1147473c31401ebcb8ad882c79a6f6f1900583aa
